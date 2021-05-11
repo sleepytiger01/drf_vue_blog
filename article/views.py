560e63fb0f0a861823aba3efe65ee6d7f75692d6
zhouhc <dontfly1986@163.com>
2021-05-11 15:57:17 +0800

    20210511,章节-过滤文章

--- article/views.py
+++ article/views.py
@@ -6,8 +6,9 @@ from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 from rest_framework import viewsets
+from rest_framework import filters
 
-from django.http import Http404
+from django_filters.rest_framework import DjangoFilterBackend
 
 from article.models import Article
 from article.permissions import IsAdminUserOrReadOnly
@@ -19,6 +20,9 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     permission_classes = [IsAdminUserOrReadOnly]
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
